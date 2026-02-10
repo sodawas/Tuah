c9b70af260d6ba063504913b033b9862ce7d4e80
sodawas <ravixmc66@gmail.com>
2026-02-10 04:50:42 +0100

    Update Blacklist.lua

--- Management/Blacklist.lua
+++ Management/Blacklist.lua
@@ -1,11 +1,6 @@
 return {
     [8049721378] = {
-        expires = 1924992000,
-        reason = "testing lol"
-    },
-
-    [1234567890] = {
-        expires = math.huge,
-        reason = "Permanent ban"
+        expires = 1999999999,
+        reason = "testing"
     }
 }
