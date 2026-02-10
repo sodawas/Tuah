8fabe128efb979ab3d3e0a59f99bb03b0d1dedd2
sodawas <ravixmc66@gmail.com>
2026-02-10 04:39:26 +0100

    Add Blacklist.lua for managing banned users

---
+++ Management/Blacklist.lua
@@ -0,0 +1,11 @@
+return {
+    [8049721378] = {
+        expires = 1924992000,
+        reason = "testing lol"
+    },
+
+    [1234567890] = {
+        expires = math.huge,
+        reason = "Permanent ban"
+    }
+}
